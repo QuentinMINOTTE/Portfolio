163f291390b4e5957a7c37490074e6ef3dbb3411
QuentinMINOTTE <150448746+QuentinMINOTTE@users.noreply.github.com>
2024-11-27 10:09:31 +0100

    Add files via upload

---
+++ competences.js
@@ -0,0 +1,8 @@
+function toggleMenu(menuId) {
+    const menu = document.getElementById(menuId);
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
